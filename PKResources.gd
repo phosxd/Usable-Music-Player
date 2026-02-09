9da52bfaebc97619a89647b6e703a0921a29b6ad
PhosXD <phosphorusxd@proton.me>
2026-02-09 07:09:47 -0500

    Init

---
+++ PKResources.gd
@@ -0,0 +1,14 @@
+@tool
+extends Node
+
+const Colors = {
+	'background': Color(0.15, 0.15, 0.15)
+}
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(_delta:float) -> void:
+	pass
---
+++ Scenes/Main/main.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_audio_finished() -> void:
+	pass # Replace with function body.
