a3c72779d390a6aa0b47412f3871449f11a316ba
PhosXD <167900279+phosxd@users.noreply.github.com>
2026-04-16 17:40:35 -0400

    Fix immersive player.

--- src/Scripts/UI/Immersive Player/immersive_player.gd
+++ src/Scripts/UI/Immersive Player/immersive_player.gd
@@ -1,38 +1,26 @@
 extends Control
 
-const margin:int = 190
+const nodes_to_hide:Array[String] = [
+	'Topbar',
+	'Sidebar',
+]
 @onready var default_shadow_style:StyleBox = %Shadow.get_theme_stylebox('panel')
 @export var overlay_color := Color(0.25, 0.25, 0.25, 0.5)
 var dominant_colors:Array[Color] = []
 
 
 func _ready() -> void:
-	var sidebar = SessionManager.main_scene.get_node('%Sidebar')
-	var topbar = SessionManager.main_scene.get_node('%Topbar') 
-	var rightbar = SessionManager.main_scene.get_node('%Right Sidebar')
-	if sidebar is Control:
-		sidebar.hide()
-	if topbar is Control:
-		topbar.hide()
-	if rightbar is Control:
-		rightbar.hide()
-
 	PlayerManager.current_track_updated.connect(update_current_track)
 	PlayerManager.track_peak_volume_changed.connect(update_visualizer)
 	update_current_track(0, PlayerManager.get_current_track())
-	_on_image_margin_item_rect_changed()
+
+	for node_name in nodes_to_hide:
+		SessionManager.main_scene.get_node('%'+node_name).hide()
 
 
 func _exit_tree() -> void:
-	var sidebar = SessionManager.main_scene.get_node('%Sidebar')
-	var topbar = SessionManager.main_scene.get_node('%Topbar')
-	var rightbar = SessionManager.main_scene.get_node('%Right Sidebar')
-	if sidebar is Control:
-		sidebar.show()
-	if topbar is Control:
-		topbar.show()
-	if rightbar is Control:
-		rightbar.show()
+	for node_name in nodes_to_hide:
+		SessionManager.main_scene.get_node('%'+node_name).show()
 
 
 func update_current_track(_track_queue_position:int, track:DBTrack) -> void:
@@ -54,7 +42,10 @@ func update_current_track(_track_queue_position:int, track:DBTrack) -> void:
 		index += 1
 		mat.set_shader_parameter(i, dominant_colors[index].blend(overlay_color))
 	var new_style = default_shadow_style.duplicate()
-	new_style.shadow_color = Color(dominant_color.r, dominant_color.g, dominant_color.b, 0.6)
+	var shadow_color:Color = dominant_color
+	shadow_color.v = 1.0
+	shadow_color.a = 0.3
+	new_style.shadow_color = shadow_color
 	new_style.shadow_size = 14
 	%Shadow.remove_theme_stylebox_override('panel')
 	%Shadow.add_theme_stylebox_override('panel', new_style)
@@ -71,23 +62,6 @@ func update_visualizer(_db:float) -> void:
 		#index += 1
 		#var i_color:Color = dominant_colors[index]
 		#mat.set_shader_parameter(i, i_color.blend(color))
-
-
-func _on_image_margin_item_rect_changed() -> void:
-	var default_size := SessionManager.default_window_size
-	var current_size := get_window().size
-	var new_margin = MathUtils.transfer_range_of_value(Vector2(0,default_size.y), Vector2(0,current_size.y), margin/2.0)
-	%'Image Margin'.add_theme_constant_override('margin_bottom', new_margin)
-	%'Image Margin'.add_theme_constant_override('margin_top', new_margin)
-	%'Image Margin'.add_theme_constant_override('margin_left', new_margin)
-	%'Image Margin'.add_theme_constant_override('margin_right', new_margin)
-
-
-func _on_image_item_rect_changed() -> void:
-	%Shadow.size = %Image.size
-	%Shadow.position = %Image.global_position
-	%Button.size = %Image.size
-	%Button.position = %Image.global_position
 
 
 func _on_button_pressed() -> void:
